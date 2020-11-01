e82bc974cdf3e965f7d2fdeff2c29366c8b0aa6c
NeverBehave <gayhub@never.pet>
2020-11-01 13:40:25 -0500

    update

--- hw5/lis.rkt
+++ hw5/lis.rkt
@@ -1,21 +1,18 @@
-;; Helpers
-(define (isNonDecreasing? ls prev)
-  (cond
-    ((null? ls) #t)
-    ((null? prev) (isNonDecreasing? (cdr ls) (car ls)))
-    ((>= (car ls) prev) (isNonDecreasing? (cdr ls) (car ls)))
-    (else #f)
-    )
-  )
-
+;; Contract: not-decrease? (list a) -> bool
+;; Purpose: if a list is non-decreasing list
+;; Example: (not-decrease? '(1 2 3 4 3 4 5)) should produce #f
+;; Definition:
 (define (firstNonDecreasing ls)
   (cond ((null? ls) #f)
-        ((isNonDecreasing? (car ls) '()) (car ls))
+        ((not-decrease? (car ls)) (car ls))
         (else (firstNonDecreasing (cdr ls)))
         )
   )
         
-
+;; Contract: subListRecursive (list originL) * number * number -> bool * (list)
+;; Purpose: recursively extend the list from given index to request length
+;; Example: (subListRecursive '(1 2 3) 0 1) yields '(1)
+;; Definition:
 (define (subListRecursive originL index requestLength)
   (if (> requestLength 0)
       (if (< index (length originL))
@@ -48,7 +45,10 @@
       )
   )
 
-
+;; Contract: lisRecursive (list originL) * number -> (list (list number))
+;; Purpose: recursively get all possible list of squence with given length
+;; Example: (lisRecursive '(1 2 3) 1) yields '((1) (2) (3))
+;; Definition:
 (define (lisRecursive originL length)
   (if (< length 0) #f
       (let (
@@ -63,7 +63,10 @@
      )
   )
 
-;; Starter
+;; Contract: (list int) -> (list int)
+;; Purpose: a list's non-decreasing sub list
+;; Example: (lis_slow '(1 2 3 4 3 4 5)) should produce '(1 2 3 4 5)
+;; Definition:
 (define (lis_slow ls)
   (if
    (null? ls)
@@ -72,61 +75,76 @@
    )
   )
 
-;; n^2 solution
-(define (lis_fast_find_all originL index currentL)
-  (if (< index (length originL))
-      (if (null? currentL)
-          (lis_fast_find_all originL (+ index 1) (cons (list-ref originL index) '()))
-          (let* (
-                (prev (car (reverse currentL)))
-                (next (list-ref originL index))
-                (nextIndex (+ index 1))
-                )
-            (if (<= prev next)
-                (let (
-                      (accept (lis_fast_find_all originL nextIndex (append currentL (cons next '()))))
-                      (drop (lis_fast_find_all originL nextIndex currentL))
-                      )
-                (lis_fast_find_all originL nextIndex currentL)
-                )
-            )
-          )
-      currentL
+;; Contract: not-decrease? (list a) -> bool
+;; Purpose: if a list is non-decreasing list
+;; Example: (not-decrease? '(1 2 3 4 3 4 5)) should produce #f
+;; Definition:
+(define (not-decrease? lst)
+  (if (< (length lst) 2) #t
+      (and (<= (car lst) (cadr lst))
+           (not-decrease? (cdr lst))
       )
   )
-                
-         
-  
+)
 
-(define (lis_fast_recursive originL index currentL)
-  (if (< index (length originL))
-      (let (
-            (calculatedLS (lis_fast_find_all originL index '()))
-            )
-        (if (< (length currentL) (length calculatedLS))
-            (lis_fast_recursive originL (+ index 1) calculatedLS)
-            (lis_fast_recursive originL (+ index 1) currentL)
-            )
-        )
-      currentL)
-  )
-        
- 
-          
-(define (lis_fast ls)
-  (if (null? ls) '()
-      (lis_fast_recursive ls 0 '())
+;; Contract: list-max (list int) * (list int) * int * int * int * int * int -> int * int
+;; Purpose: helper function for lis_fast_helper, find the max in top n of a list
+; for all s in str that less or equal than smax
+; return the max value and the index of it
+; initial index is 0
+; initial max-value is 1
+; initial max-index is index
+;; Example: (list-max '(1 2 3 2 4 1 2) '() 0 0 1 1 -1) should produce (1 -1)
+;; Definition:
+(define (list-max str val index end-index max-str max-value max-index)
+  (if (eq? index end-index)
+      (list max-value max-index)
+      (if (and (<= (list-ref str index) max-str) (> (+ (list-ref val index) 1) max-value))
+          (list-max str val (+ index 1) end-index max-str (+ (list-ref val index) 1) index)
+          (list-max str val (+ index 1) end-index max-str max-value max-index)
       )
   )
-  
+)
 
+;; Contract: (list int) * int * int * (list int) * (list int) * int * int -> (list int) * int
+;; Purpose: helper function for lis_fast, using dynamic programming
+; str is the list
+; initial index is 0
+; val-acc: store the length
+; loc-acc: store the index of the result
+; max-value: the value of the max
+; max-index the index of the max
+; next value: (car result)
+; next loc: (cadr result)
+;; Example (lis_fast_helper (1 2 3 2 4 1 2) 0 () () 0 -1) should produce ((-1 0 1 1 2 0 3) 4)
+;; Definition:
+(define (lis_fast_helper str index val-acc loc-acc max-value max-index)
+  (if (eq? index (length str)) (list loc-acc max-index)
+   (let ((result (list-max str val-acc 0 index (list-ref str index) 1 -1)))
+     (lis_fast_helper str (+ index 1) (append val-acc (list (car result))) (append loc-acc (list (cadr result)))
+                      (max (car result) max-value) (if (> (car result) max-value) index max-index))
+   )
+  )
+)
 
-;; Test Case
-;(define list1 '(1 2 3 2 4 1 2))
-;(define list2 '(2 4 3 1 2 1))
-;(lis_slow list1)
-;(lis_slow list2)
-;(lis_fast list1)
-;(lis_fast list2)
-;(subListRecursive list1 0 7)
-;(firstNonDecreasing '((1 2 3 2 4 1 2)))
+;; Contract: (list int) * (list int) * int -> (list int)
+;; Purpose: get result from str, loc-acc and index
+;; Example (get-result (1 2 3 2 4 1 2) (-1 0 1 1 2 0 3) 4) should produce (1 2 3 4)
+;; Definition:
+(define (get-result str loc-acc index)
+  (if (equal? index -1)
+      '()
+      (cons (list-ref str index)
+            (get-result str loc-acc (list-ref loc-acc index)))
+  )
+)
+
+;; Contract: (list int) -> (list int)
+;; Purpose: a list's non-decreasing sub list
+;; Example: (lis_fast '(1 2 3 4 3 4 5)) should produce '(1 2 3 4 5)
+;; Definition:
+(define (lis_fast str) '()
+ (let ((result (lis_fast_helper str 0 '() '() 0 -1)))
+    (reverse (get-result str (car result) (cadr result)))
+ )
+)
